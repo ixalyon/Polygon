472a1dd76c38a6d05971d84d6645045a7fcfa90d
Sathindra Kuruppu <kaskuruppu@gmail.com>
2017-12-19 00:40:02 +0530

    Test loop

--- src/Forex.java
+++ src/Forex.java
@@ -1,23 +1,30 @@
 import java.io.IOException;
 
+import us.monoid.json.JSONException;
+import us.monoid.web.JSONResource;
 import us.monoid.web.Resty;
 
 
 public class Forex {
-    public static void main(String[] args) throws IOException {
-    //	for(int i = 0 ; i < 100 ; i++)
-    		CurrencyQuotes("GBP/USD", "Xs9_inK7TbwPZuzkl_4grrKh6mn8YB18IdnCEI");
+    public static void main(String[] args) throws IOException, JSONException {
+    	long time=0;
+    	String s;
+		for(int i = 0 ; i < 100 ; i++) {
+	    		s =CurrencyQuotes("GBP/USD", "Xs9_inK7TbwPZuzkl_4grrKh6mn8YB18IdnCEI");
+	    		System.out.println(time-(time=Long.parseLong(s)));
+		}
     }
 
 
-	static void CurrencyQuotes(String pair,String api) throws IOException{
+	static String CurrencyQuotes(String pair,String api) throws IOException, JSONException{
 		StringBuilder sb = new StringBuilder("https://api.polygon.io/v1/last_quote/currencies/"+pair+"?apiKey="+api);
-
 		Resty r = new Resty();
 
-		Object name = r.text(sb.toString());
-		System.out.println(name.toString());
+		JSONResource name = r.json(sb.toString());
+		String s=name.toObject().getJSONObject("last").get("timestamp").toString();
+//		System.out.println(s);
 		
+		return s;
 	}
 
 }
